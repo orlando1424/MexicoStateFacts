6959415bb1e8c597aadefbf81a4bd668776a7e10
Orlando <53872517+orlando1424@users.noreply.github.com>
2021-07-15 09:09:43 -0600

    init commit

---
+++ MexicoStateFacts/MexicoStateFacts/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MexicoStateFacts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
