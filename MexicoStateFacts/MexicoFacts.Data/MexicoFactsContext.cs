c5b71a5d718fb4cde69a30925598885aaa55bef7
Orlando <53872517+orlando1424@users.noreply.github.com>
2021-07-15 10:00:26 -0600

    added dbcontext

---
+++ MexicoStateFacts/MexicoFacts.Data/MexicoFactsContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MexicoFacts.Domain;
+
+namespace MexicoFacts.Data
+{
+    public class MexicoFactsContext : DbContext
+    {
+        public DbSet<State> States { get; set; }
+        public DbSet<Fact> Facts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; InitialCatalog=MexicoFactsData");
+           
+        }
+    }
+}
